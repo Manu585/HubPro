520d4c855b717420a99dedc24d31ebaf1717710d
Manu585 <manuelgogg2@gmail.com>
2024-03-22 22:08:13 +0100

    FirstCommit

---
+++ src/main/java/at/manu/hubpro/HubPro.java
@@ -0,0 +1,17 @@
+package at.manu.hubpro;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HubPro extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
