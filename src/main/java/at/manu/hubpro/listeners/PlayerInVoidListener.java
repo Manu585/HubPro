784f950018623e733918b4ce550c4690d3ec7c82
SpeedyGo55 <83915114+SpeedyGo55@users.noreply.github.com>
2024-03-24 17:28:21 +0100

    added height restriction

--- src/main/java/at/manu/hubpro/listeners/PlayerInVoidListener.java
+++ src/main/java/at/manu/hubpro/listeners/PlayerInVoidListener.java
@@ -1,9 +1,21 @@
 package at.manu.hubpro.listeners;
 
+import at.manu.hubpro.configuration.ConfigManager;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+import java.util.Objects;
 
 public class PlayerInVoidListener implements Listener {
     @EventHandler
-    public void onPlayerMove()
+    public void onPlayerMove(PlayerMoveEvent e) {
+        int voidY = ConfigManager.defaultConfig.get().getInt("HubPro.VoidY");
+        Player player = e.getPlayer();
+        if (player.getLocation().getY() < voidY) {
+            player.sendMessage(Objects.requireNonNull(ConfigManager.languageConfig.get().getString("HubPro.Chat.VoidMessage")));
+            player.teleport(player.getWorld().getSpawnLocation());
+        }
+    }
 }
