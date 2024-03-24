53cdada7d1fb21d724b535bb9d503e5034eac643
SpeedyGo55 <83915114+SpeedyGo55@users.noreply.github.com>
2024-03-24 17:18:48 +0100

    Added my branch and started working on height restriction

---
+++ src/main/java/at/manu/hubpro/listeners/PlayerInVoidListener.java
@@ -0,0 +1,9 @@
+package at.manu.hubpro.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class PlayerInVoidListener implements Listener {
+    @EventHandler
+    public void onPlayerMove()
+}
